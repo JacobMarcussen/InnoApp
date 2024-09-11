3d11402d590538434b1ad245a1024eac07691ae6
ValdemarEbbeskov <131356601+ValdemarEbbeskov@users.noreply.github.com>
2024-09-11 14:28:57 +0200

    Update App.js

--- Exercise/Exercise-inno/App.js
+++ Exercise/Exercise-inno/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Det spiller maks</Text>
+      <Text>Det spiller stadig maks</Text>
       <StatusBar style="auto" />
     </View>
   );
